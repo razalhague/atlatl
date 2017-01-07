58ee836d00af094a0906990938809ed42b00aeb8
Ville Jokela <ville.jokela@penny-craal.org>
2017-01-07 14:57:18 +0200

    display error message

---
+++ src/main/scala/org/penny_craal/atlatl/AtlatlSsc.scala
@@ -0,0 +1,43 @@
+/* Copyright © 2016 Ville Jokela
+ *
+ * This file is part of atlatl.
+ *
+ * atlatl is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * atlatl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with atlatl.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * contact me <ville.jokela@penny-craal.org>
+ */
+
+package org.penny_craal.atlatl
+
+import javax.swing.JOptionPane
+
+import akka.actor.SupervisorStrategy.Escalate
+import akka.actor.{OneForOneStrategy, SupervisorStrategy, SupervisorStrategyConfigurator}
+
+/**
+  * @author Ville Jokela
+  */
+class AtlatlSsc extends SupervisorStrategyConfigurator {
+  override def create(): SupervisorStrategy = OneForOneStrategy() {
+    case e: Exception =>
+      def throwableToStringWithCause(t: Throwable): String = {
+        if (t.getCause != null)
+          t.toString + "\n" + throwableToStringWithCause(t.getCause)
+        else
+          t.toString
+      }
+      JOptionPane.showMessageDialog(null, throwableToStringWithCause(e), "Error", JOptionPane.ERROR_MESSAGE)
+      Escalate
+  }
+}
